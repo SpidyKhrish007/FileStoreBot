4992c69e9f8a7f3c180c175cf2a384ab1b56c46b
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-08-30 17:30:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+API_ID = int(os.environ.get("API_ID", '25947375'))
+API_HASH = os.environ.get("API_HASH", 'af7b9b8a0d377b4d3735199e9df008af')
+BOT_TOKEN = os.environ.get("BOT_TOKEN", '6331021246:AAF9a2NzaPBpG3ZvP7BvfelDJuA7SlkunYY')
 DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+OWNER_ID = int(os.environ.get("6408116706"))
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '-1001629572693')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
